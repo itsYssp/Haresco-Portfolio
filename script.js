099637f5f6658e23f6dac82e138633dfbefeaec0
itsYssp <167275515+itsYssp@users.noreply.github.com>
2024-05-28 22:29:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sideMenu =document.querySelector('.sideMenu' );
+
+function galaw(){
+    sideMenu.style.right= "0";
+    
+}
+
+function alis(){
+    sideMenu.style.right= "-200px"; 
+    
+}
+
